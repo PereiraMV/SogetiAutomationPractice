bb0bde0e907a30a642a1feca54dbfe49ac1ff1d5
marc <pereiramarc@hotmail.com>
2021-06-02 15:46:15 +0200

    Implemented all HomePage functions and crearted a new class for the description of a product

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,7 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class HomePage {
     WebDriver driver;
+    By buttonAccountClassSelector = By.className("account");
+    By buttonCartClassSelector = By.className("shopping_cart");
+    By searchBarIdSelector = By.id("search_query_top");
+    By buttonSearchcssSelector = By.cssSelector("[name='submit_search']");
+    By resultSearchAutoCompleteClassSelector = By.className("ac_results");
+    By resultListItemCssSelector = By.cssSelector("li");
 
     public HomePage(WebDriver driver){
         this.driver = driver;
@@ -9,21 +22,43 @@ public class HomePage {
 
     public AuthenticationPage openSignInPage(){
 
+        driver.findElement(buttonAccountClassSelector).click();
         return new AuthenticationPage(driver);
     }
 
     public CartPage openCartPage(){
 
+        driver.findElement(buttonCartClassSelector).click();
         return new CartPage(driver);
     }
 
-    public ProductListPage Search(){
+    public ProductListPage search(String elementSearched){
 
+        driver.findElement(searchBarIdSelector).sendKeys(elementSearched);
+        driver.findElement(buttonSearchcssSelector).click();
         return new ProductListPage(driver);
     }
 
-    public ProductListPage SearchByAutoComplete(){
-        return new ProductListPage(driver);
+    public ProductDescriptionPage searchByAutoComplete(String elementSearched, int index){
+        driver.findElement(searchBarIdSelector).sendKeys(elementSearched);
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        WebElement listResult = wait.until( ExpectedConditions.elementToBeClickable(resultSearchAutoCompleteClassSelector));
+
+        if (listResult == null){
+
+        }
+
+        List<WebElement> ListItemInList = listResult.findElements(resultListItemCssSelector);
+
+        Integer numberItem = ListItemInList.size();
+        System.out.println(numberItem);
+        // Regarder la taille du tableau
+        ListItemInList.get(0).click();
+
+
+        return new ProductDescriptionPage(driver);
     }
 
 
---
+++ src/main/java/ProductDescriptionPage.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ProductDescriptionPage {
+    WebDriver driver;
+
+    public ProductDescriptionPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+}
